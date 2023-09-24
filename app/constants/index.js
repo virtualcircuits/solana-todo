c0e1fc526870842dff28d3668a3fc79a20fa34a1
kali <kali@kali.kali>
2023-09-25 06:23:11 +0800

    Improved App 092523

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("EMRELEkPnwDNsuyHmYZYmMroJ69FpdAfSgDHrTxEYrq3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("8wfThCrcT3CTC56NrQiyZUbYUmpAsEqr9Hp3CcUEsWfM");
