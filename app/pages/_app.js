2bb7d1c4d2843c0847b651bc6ae95447dc209f62
virtualcircuits <lozada_paolo@yahoo.com>
2023-09-25 05:58:54 +0800

    Improved App
     Author:    virtualcircuits <lozada_paolo@yahoo.com>
     Date:      Mon Sep 25 05:58:54 2023 +0800

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -4,21 +4,30 @@ import '../styles/global.css'
 import { WalletConnectProvider } from '../components/WalletConnectProvider'
 // Import the solana wallet css
 import '@solana/wallet-adapter-react-ui/styles.css'
+ 
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Head>
-                <title>Todo App</title>
-            </Head>
+            
+            <div style={{ textAlign: 'center', color: 'white', fontSize: '40px' }}>Solana Blockchain</div>
+            <div style={{ textAlign: 'center', color: 'white', fontSize: '30px' }}>To Do App</div>
+
             <main>
                 {/* Wrap provider around App */}
                 <WalletConnectProvider>
                     <Component {...pageProps} />
                 </WalletConnectProvider>
             </main>
+           
         </>
     )
 }
 
 export default MyApp
+
+
+
+
+
+
